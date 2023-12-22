bc666631d682e378b1566db7971e98e4afbddf90
ChristSoler1 <144997362+ChristSoler1@users.noreply.github.com>
2023-12-22 16:34:16 +0100

    Delete AMSS Project/kanban-backend/kanban-backend directory

--- AMSS Project/kanban-backend/kanban-backend/src/main/java/com/cb/kanbanbackend/exception/CommonException.java
+++
@@ -1,9 +0,0 @@
-package com.cb.kanbanbackend.exception;
-
-//Placeholder for Exceptions
-
-public class CommonException extends RuntimeException {
-    public CommonException(String message) {
-        super(message);
-    }
-}
--- AMSS Project/kanban-backend/kanban-backend/src/main/java/com/cb/kanbanbackend/exception/NoDataFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cb.kanbanbackend.exception;
-
-// In case no data is found
-
-public class NoDataFoundException extends RuntimeException {
-    public NoDataFoundException(String message) {
-        super(message);
-    }
-}
